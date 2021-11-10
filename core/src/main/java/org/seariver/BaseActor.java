130a3ce20621ac59e6df2a895c1dff15311a8d3e
Mario Rezende <mariorez@gmail.com>
2021-11-10 17:58:27 -0300

    import cleanup

--- core/src/main/java/org/seariver/BaseActor.java
+++ core/src/main/java/org/seariver/BaseActor.java
@@ -18,7 +18,6 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.viewport.Viewport;
 
 import java.util.ArrayList;
 
